da0c82c52f43890c20d1f3feecb87f0508035917
Joel Chubrega <jchubrega@gmail.com>
2025-07-31 13:56:04 -0300

    fix

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -5,7 +5,7 @@ const ItemListContainer = ({ saludo }) => {
   return (
     <section className="flex flex-1 justify-center">
       <h2 className="text-3xl pt-8">{saludo}</h2>
-      < ItemList />
+      <ItemList />
     </section>
   );
   
